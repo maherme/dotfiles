c68ee9ec023e53a672b047d77ebbf9674ede675a
maherme <maherme.dev@gmail.com>
2025-11-19 18:53:57 +0100

    Add dotfiles

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,31 @@
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+    signs_staged = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+      local map = function(mode, lhs, rhs, desc)
+        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+      end
+
+      map('n', ']c', function() gs.next_hunk() end, 'Git next hunk')
+      map('n', '[c', function() gs.prev_hunk() end, 'Git prev hunk')
+    end,
+  },
+}
